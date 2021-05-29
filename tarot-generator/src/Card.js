90e9738ab36824e7b0e8dcdf8a7d84e769a3c4c4
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-05-28 23:22:05 -0500

    Adds map of data returned, adds modular card count, moves drawCard out of clickHandler function

--- tarot-generator/src/Card.js
+++ tarot-generator/src/Card.js
@@ -1,28 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Card-details.css";
 
 const tarotUrl = "https://rws-cards-api.herokuapp.com/api/v1/cards";
+const cardCount = 4;
 
 const Card = () => {
-  const [cardData, setCardData] = useState({});
-  const handleFetch = (e) => {
+  const [cardData, setCardsData] = useState([]);
+
+  const drawCards = () => {
     axios
-      .get(`${tarotUrl}/random?n=1`)
+      .get(`${tarotUrl}/random?n=${cardCount}`)
       .then((res) => {
-        setCardData(res.data.cards[0]);
-        console.log(res.data.cards[0]);
+        setCardsData(res.data.cards);
+        console.log(res.data.cards);
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
+  const handleFetch = (e) => {
+    e.preventDefault();
+    drawCards();
+  };
+
   return (
     <div>
-      <header className="Card-header">{cardData.name}</header>
-      <div className="Meaning-holder">
-        <p className="Card-meaning">{cardData.meaning_up}</p>
-      </div>
+      {cardData.map((card) => {
+        return <header className="Card-header">{card.name}</header>;
+      })}
       <button onClick={handleFetch}>Fetch a card</button>
     </div>
   );
