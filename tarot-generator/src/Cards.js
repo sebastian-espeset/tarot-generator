65e4aa400be8480af224dcddb31726e3452f8da3
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-06-05 13:55:11 -0500

    cards are laid out correctly, slight border, toggling works

--- tarot-generator/src/Cards.js
+++ tarot-generator/src/Cards.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import './card.css';
 
 const tarotUrl = "https://rws-cards-api.herokuapp.com/api/v1/cards";
 const cardCount = 5;
@@ -33,88 +34,95 @@ const Cards = () => {
       ...cardReveal,
       [e.target.getAttribute("name")]: !cardReveal[targetedCard],
     });
-    console.log(targetedCard)
   };
 
   return (
-    <div className="container">
-      <div className="columns">
-        <div className="column is-offset-one-third">
+    
+    <div className="container is-cards-container">
+      <div className="tile is-ancestor">
+      <div className="tile is-4"></div>{/*This is a blank tile for adding space */}
+      
+        <div className="tile is-4 is-bordered">
           <div
             onClick={handleCardReveal}
             name="card_one"
             className={
-              cardReveal.card_one ? "title" : "title has-background-danger"
+              cardReveal.card_one ? "content is-large" : "content has-background-danger is-large"
             }
           >
             {cardData[0] ? cardData[0].name : "This is a cards title"}
-          </div>
           <div className="content">
             {cardData[0] ? cardData[0].meaning_up : "this is a cards meaning"}
           </div>
+          </div>
         </div>
       </div>
-      <div className="columns">
-        <div className="column">
+      <div className="tile is-4"></div>
+      <div className="tile is-ancestor">
+        <div className="tile is-4 is-bordered" onClick={handleCardReveal}>
           <div
-            onClick={handleCardReveal}
+            
             className={
-              cardReveal.card_two ? "title" : "title has-background-danger"
+              cardReveal.card_two ? "content is-large" : "content has-background-danger is-large"
             }
             name="card_two"
           >
             {cardData[1] ? cardData[1].name : "This is a cards title"}
-          </div>
           <div className="content">
             {cardData[1] ? cardData[1].meaning_up : "this is a cards meaning"}
           </div>
+          </div>
         </div>
-        <div className="column">
+        <div className="tile is-4 is-bordered" onClick={handleCardReveal}>
           <div
-            onClick={handleCardReveal}
+            
             className={
-              cardReveal.card_three ? "title" : "title has-background-danger"
+              cardReveal.card_three
+                ? "content is-large"
+                : "content has-background-danger is-large"
             }
             name="card_three"
           >
             {cardData[2] ? cardData[2].name : "This is a cards title"}
-          </div>
           <div className="content">
             {cardData[2] ? cardData[2].meaning_up : "This is a cards meaning"}
           </div>
+          </div>
         </div>
-        <div className="column">
+        <div className="tile is-4 is-bordered"  onClick={handleCardReveal}>
           <div
-            onClick={handleCardReveal}
+           
             className={
-              cardReveal.card_four ? "title" : "title has-background-danger"
+              cardReveal.card_four ? "content is-large" : "content has-background-danger is-large"
             }
             name="card_four"
           >
             {cardData[3] ? cardData[3].name : "This is a cards title"}
-          </div>
           <div className="content">
             {cardData[3] ? cardData[3].meaning_up : "This is a cards meaning"}
           </div>
+          </div>
         </div>
       </div>
-      <div className="columns">
-        <div className="column is-offset-one-third">
+       <div className="tile is-ancestor">
+      <div className="tile is-4"></div>
+        <div className="tile is-4 is-bordered" onClick={handleCardReveal}>
           <div
-            onClick={handleCardReveal}
+            
             className={
-              cardReveal.card_five ? "title" : "title has-background-danger"
+              cardReveal.card_five ? "content is-large" : "content has-background-danger is-large"
             }
             name="card_five"
           >
             {cardData[4] ? cardData[4].name : "This is a cards title"}
-          </div>
-          <div className="content">
-            {cardData[4] ? cardData[4].meaning_up : "This is a cards meaning"}
+            <div className="content">
+              {cardData[4] ? cardData[4].meaning_up : "This is a cards meaning"}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className = "tile is-4"></div>
+      </div>
   );
 };
 export default Cards;
