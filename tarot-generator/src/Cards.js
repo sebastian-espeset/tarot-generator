138943e6a1fd829e44c84ae6f9487ada85f337d5
Sebastian Espeset <Seba@Sebastians-MacBook-Pro.local>
2021-07-11 18:12:25 -0500

    Adds draw card button, and redraw cards

--- tarot-generator/src/Cards.js
+++ tarot-generator/src/Cards.js
@@ -1,17 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Card, CardContent, Grid, Typography} from "@material-ui/core";
+import { Card, CardContent, Grid, Typography, Button } from "@material-ui/core";
 const tarotUrl = "https://rws-cards-api.herokuapp.com/api/v1/cards";
 const cardCount = 5;
 
 const Cards = () => {
-  const initialCardReveal = {
-    card_one: false,
-    card_two: false,
-    card_three: false,
-    card_four: false,
-    card_five: false,
-  };
+  let initialCardReveal = false;
+
   const [cardData, setCardsData] = useState([]);
   const [cardReveal, setCardReveal] = useState(initialCardReveal);
 
@@ -19,78 +14,109 @@ const Cards = () => {
     axios
       .get(`${tarotUrl}/random?n=${cardCount}`)
       .then((res) => {
-        console.log(res.data.cards)
+        console.log(res.data.cards);
         setCardsData(res.data.cards);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+  
+  function redrawCards(){
+    axios
+      .get(`${tarotUrl}/random?n=${cardCount}`)
+      .then((res) => {
+        console.log(res.data.cards);
+        setCardsData(res.data.cards);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
+  function cardRevealToggle() {
+    setCardReveal(!initialCardReveal);
+  }
 
-  const handleCardReveal = (e) => {
-    const targetedCard = e.target.getAttribute("name");
-    setCardReveal({
-      ...cardReveal,
-      [e.target.getAttribute("name")]: !cardReveal[targetedCard],
-    });
-  };
-
+  if (cardReveal) {
+    return (
+      <>
+      <Button onClick={redrawCards}>Re-draw?</Button>
+        <Grid container spacing={3} padding="10px" alignItems="center">
+          <Grid item xs={2}>
+            <Card>
+              <CardContent>
+                <Typography variant="title">
+                  {cardData[0] ? cardData[0].name : "Loading "}
+                </Typography>
+              </CardContent>
+              <Typography padding="10px" variant="content1">
+                {cardData[0] ? cardData[0].meaning_up : "Loading"}
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card></Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card>
+              <CardContent>
+                <Typography variant="title">
+                  {cardData[1] ? cardData[1].name : "Loading"}
+                </Typography>
+              </CardContent>
+              <Typography padding="10px" variant="content1">
+                {cardData[1] ? cardData[1].meaning_up : "Loading"}
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card>
+              <CardContent>
+                <Typography variant="title">
+                  {cardData[2] ? cardData[2].name : "Loading"}
+                </Typography>
+              </CardContent>
+              <Typography padding="10px" variant="content1">
+                {cardData[2] ? cardData[2].meaning_up : "Loading"}
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card>
+              <CardContent>
+                <Typography variant="title">
+                  {cardData[3] ? cardData[3].name : "Loading"}
+                </Typography>
+              </CardContent>
+              <Typography padding="10px" variant="content1">
+                {cardData[3] ? cardData[3].meaning_up : "Loading"}
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card>
+              <CardContent>
+                <Typography variant="title">
+                  {cardData[4] ? cardData[4].name : "Loading"}
+                </Typography>
+              </CardContent>
+              <Typography padding="10px" variant="content1">
+                {cardData[4] ? cardData[4].meaning_up : "Loading"}
+              </Typography>
+            </Card>
+          </Grid>
+        </Grid>
+      </>
+    );
+  }
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={6} align="center">
-          <Card>
-            <CardContent>
-              <Typography>
-                {cardData[0] ? cardData[0].name : "Loading"}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item>
-          <Card>
-
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardContent>
-              <Typography>
-              {cardData[1] ? cardData[1].name : "Loading"}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardContent>
-              <Typography>
-              {cardData[2] ? cardData[2].name : "Loading"}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardContent>
-              <Typography>
-              {cardData[3] ? cardData[3].name : "Loading"}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardContent>
-              <Typography>
-              {cardData[4] ? cardData[4].name : "Loading"}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+    <Button onClick={cardRevealToggle}> 
+      draw Cards 
+    </Button>
     </>
   );
 };
-    
+
 export default Cards;
