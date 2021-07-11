40a20ce513187b01ba1d9b62679b0cb305061aa9
Sebastian Espeset <Seba@Sebastians-MacBook-Pro.local>
2021-07-11 11:59:51 -0500

    added grid, grid item, and cards

--- tarot-generator/src/Cards.js
+++ tarot-generator/src/Cards.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import {Card, CardContent, Grid, Typography} from "@material-ui/core";
 const tarotUrl = "https://rws-cards-api.herokuapp.com/api/v1/cards";
 const cardCount = 5;
 
@@ -19,6 +19,7 @@ const Cards = () => {
     axios
       .get(`${tarotUrl}/random?n=${cardCount}`)
       .then((res) => {
+        console.log(res.data.cards)
         setCardsData(res.data.cards);
       })
       .catch((err) => {
@@ -35,10 +36,61 @@ const Cards = () => {
   };
 
   return (
-    
-    <div>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={6} align="center">
+          <Card>
+            <CardContent>
+              <Typography>
+                {cardData[0] ? cardData[0].name : "Loading"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card>
 
-    </div>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography>
+              {cardData[1] ? cardData[1].name : "Loading"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography>
+              {cardData[2] ? cardData[2].name : "Loading"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography>
+              {cardData[3] ? cardData[3].name : "Loading"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card>
+            <CardContent>
+              <Typography>
+              {cardData[4] ? cardData[4].name : "Loading"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   );
 };
+    
 export default Cards;
